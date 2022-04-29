f087a1f3f07db210df05a66bcda73d72fb4bf1e5
unknown <tomoaki.0110.1998@gmail.com>
2022-04-29 20:49:21 +0900

    initial commit

---
+++ sample.js
@@ -0,0 +1,12 @@
+console.log("it test!")
+function createElement(message = "ワールド") {
+    const template = `<p>
+      hello ${message}
+    </p>`
+    return template
+}
+
+const message = createElement("world")
+
+const contents = document.getElementsByClassName("contents")[0]
+contents.innerHTML = message
